7a3f5adf9a015e35627ef7b4bb1c3303392299b9
miloslazic1999 <miloslazic1999@gmail.com>
2020-12-02 21:00:08 +0100

    Done

--- PresentationLayer/Form1.cs
+++ PresentationLayer/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer;
+using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,36 @@ namespace PresentationLayer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listBoxStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadForm(object sender, EventArgs e)
+        {
+            StudentBusiness Conn = new StudentBusiness();
+            listBoxStudents.Items.Clear();
+            foreach(Student s in Conn.GetAllStudents())
+            {
+                listBoxStudents.Items.Add(s);
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+            s.Name = textBoxName.Text;
+            s.Index = textBoxIndex.Text;
+            s.Average = decimal.Parse(textBoxAverage.Text);
+            StudentBusiness Conn = new StudentBusiness();
+            Conn.EnableInsert(s);
+            listBoxStudents.Items.Clear();
+            foreach (Student t in Conn.GetAllStudents())
+            {
+                listBoxStudents.Items.Add(t);
+            }
         }
     }
 }
---
+++ PresentationLayerWeb/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PresentationLayerWeb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StudentBusiness Conn = new StudentBusiness();
+            listBoxStudents.Items.Clear();
+            foreach (Student s in Conn.GetAllStudents())
+            {
+                listBoxStudents.Items.Add(s.ToString());
+            }
+        }
+    }
+}
